4ba5729f9046ba424ee6bb7f5ddbe5b36cd5504c
Cupcake7queenie <163912302+Cupcake7queenie@users.noreply.github.com>
2025-01-31 21:37:32 +0200

    Added a section
    book your party section

--- food-website-project/func.js
+++ food-website-project/func.js
@@ -1,1 +1,9 @@
 //add a Language selector
+document.getElementById('changeLanguageBtn').addEventListener('click', function() {
+    const greeting = document.getElementById('greeting');
+    if (greeting.innerText === 'Hello, World!') {
+        greeting.innerText = '¡Hola, Mundo!';
+    } else {
+        greeting.innerText = 'Hello, World!';
+    }
+});
