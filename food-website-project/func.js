7ba1d742b0aa521bffcf00f162f45bc0ef537594
Cupcake7queenie <163912302+Cupcake7queenie@users.noreply.github.com>
2025-02-03 19:53:45 +0200

    Added a Delivery section
    fixed a few small problems and added a delivery section

--- food-website-project/func.js
+++ food-website-project/func.js
@@ -1,9 +1,14 @@
-//add a Language selector
-document.getElementById('changeLanguageBtn').addEventListener('click', function() {
-    const greeting = document.getElementById('greeting');
-    if (greeting.innerText === 'Hello, World!') {
-        greeting.innerText = '¡Hola, Mundo!';
-    } else {
-        greeting.innerText = 'Hello, World!';
-    }
-});
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
