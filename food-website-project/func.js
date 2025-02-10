71ca9c6d34e059db550089ad958b87b24a4458d9
Cupcake7queenie <163912302+rumzdeveloper@users.noreply.github.com>
2025-02-10 21:41:43 +0200

    Touch ups
    Fixed some minor css and html issues, improved a few things to make it more user friendly and added some javascript functionality to buttons.

--- food-website-project/func.js
+++ food-website-project/func.js
@@ -1,5 +1,15 @@
+//menu.html
 function scrollToForm() {
     const form = document.getElementById('order-form');
     form.scrollIntoView({ behavior: 'smooth' }); // Smooth scroll to the form
 }
 
+
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('submit-button');
+    if (button) {
+        button.addEventListener('click', function() {
+            alert('Order submitted!'); // Display message on button click
+        });
+    }
+});
